812f1df6f40387a18a82486ee481def9394b9a8d
rehellinen <912377791@qq.com>
2018-12-26 14:03:38 +0800

    vue env

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    tree
+</template>
+
+<script>
+import Tree from './components/Tree'
+
+export default {
+  components: {
+    Tree
+  }
+}
+</script>
+
+<style>
+  @import "./assets/css/reset.css";
+</style>
